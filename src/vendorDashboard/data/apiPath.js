29031e8b6f2a6e4580aade9bff21471d9e071773
rok-web <avalavinay7@gmail.com>
2025-06-04 11:42:18 +0530

    Updated project before  deployment

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,4 @@
 //export const API_URL = "http://localhost:5000"
 
-export const API_URL = "https://backend-nodejs-suby-1-54sr.onrender.com"
+
+export const API_URL = "https://backend-nodejs-suby-ceww.onrender.com/"

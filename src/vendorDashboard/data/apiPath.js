557d195c39802c296b6a3429d0d50a08c52a6465
Avala vinay <avalavinay4@gmail.com>
2025-08-26 00:47:40 +0530

    Update apiPath.js

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,5 @@
 // export const API_URL = "http://localhost:5000"
 
-export const API_URL = "https://backend-nodejs-suby-2-rts0.onrender.com";
+REACT_APP_API_URL=https://backend-nodejs-suby-2-rts0.onrender.com
+
 

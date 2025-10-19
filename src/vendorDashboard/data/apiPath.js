aece6cff9eb102e3af4ab9835c17982e9b68736e
Avala vinay <avalavinay4@gmail.com>
2025-10-19 18:30:39 +0530

    Remove trailing slash from API_URL

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:5000"
 
-export const API_URL="https://backend-nodejs-suby-2-rts0.onrender.com/"
+export const API_URL="https://backend-nodejs-suby-2-rts0.onrender.com"
 
 
 

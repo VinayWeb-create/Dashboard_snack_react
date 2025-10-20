bd2882c96c6a929c6ce3843d605a8a70f94e1413
Avala vinay <avalavinay4@gmail.com>
2025-10-20 16:02:11 +0530

    Add user details option to sidebar

--- src/vendorDashboard/components/SideBar.jsx
+++ src/vendorDashboard/components/SideBar.jsx
@@ -4,15 +4,16 @@ const SideBar = ({
   showFirmHandler,
   showProductHandler,
   showAllProductsHandler,
-  showFirmTitle
+  showFirmTitle,
+  showUserDetailsHandler
 }) => {
   return (
     <div className="sideBarSection">
       <ul>
-        {showFirmTitle ? <li onClick={showFirmHandler}>Add Firm</li> : "" }
+        {showFirmTitle && <li onClick={showFirmHandler}>Add Firm</li>}
         <li onClick={showProductHandler}>Add Product</li>
         <li onClick={showAllProductsHandler}>All Products</li>
-        <li>User Details</li>
+        <li onClick={showUserDetailsHandler}>User Details</li>
       </ul>
     </div>
   );
